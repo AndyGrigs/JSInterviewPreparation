bd93d4f494268192e68cffcaaf90a17e9be1aaef
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2023-03-01 10:46:10 +0000

    adding capitalizeWord function

--- script_1.js
+++ script_1.js
@@ -15,5 +15,41 @@ function altCaps(str) {
 
 
 
-console.log(altCaps("I'm almost done to get"))
-console.log(altCaps("let us have your thoughts about our relationship"));
+//console.log(altCaps("I'm almost done to get"))
+//console.log(altCaps("let us have your thoughts about our relationship"));
+
+/* toTitleCase
+Write a function that will capitalize every word in a sentence.  
+
+Example Input: "everything, everywhere, all at once"
+Example Output: "Everything, Everywhere, All At Once"
+*/
+
+/* 
+First, write a function that takes in one word and 
+capitalizes the first letter of that word.
+
+Example Input: "scrimba"
+Example Output: "Scrimba"
+
+Hint: Trying using slice() and .toUpperCase()
+*/
+
+function capitalizeWord(word){
+    return word[0].toUpperCase() + word.slice(1);
+}
+
+/* 
+Now write a function that capitalizes every word in a sentence. 
+How can you reuse the function you just wrote? 
+*/ 
+
+function toTitleCase(str){
+  return str.split(" ").map(word=>{
+    return capitalizeWord(word)
+  }).join(" ")
+}
+
+// Test your functions
+console.log(capitalizeWord("pumpkin"));
+console.log(toTitleCase("pumpkin pranced purposefully across the pond"));
