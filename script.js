74eb506448061700ad0dc7bfbaf6e8dfb4b36013
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2023-03-07 09:25:43 +0000

    adding isAnagram function

--- script.js
+++ script.js
@@ -126,3 +126,19 @@ console.log(emojifyWord("elephant"));
 
 console.log(emojifyPhrase("I :heart: my :cat:"));
 console.log(emojifyPhrase("I :heart: my :elephant:"));
+
+
+
+// function for finding anagram
+function isAnagram(str1, str2) {
+   if (str1.length !== str2.length) {
+      return false
+   }
+   const newStr1 = str1.split('').sort().join('')
+   const newStr2 = str2.split('').sort().join('')
+  return newStr1 === newStr2
+}
+
+
+console.log(isAnagram("inch", "chin"));
+console.log(isAnagram("night", "thing"));
